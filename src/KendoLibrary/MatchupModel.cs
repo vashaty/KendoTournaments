2176a211a74ff932385e9c9479432f0ec2d8f047
vashaty <v_toncer@utb.cz>
2022-04-24 22:30:03 +0200

    models

---
+++ src/KendoLibrary/MatchupModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KendoTournaments
+{
+    public class MatchupModel
+    {
+        public int Id { get; set; }
+        public List<MatchupPlayerModel> Players { get; set; }
+
+        public PlayerModel Winner { get; set; }
+
+        public DateTime Datetime { get; set; }
+
+        public int MatchupRound { get; set; }
+
+        public GroupModel Group { get; set; }
+
+        public bool Overtime { get; set; }
+    }
+}
